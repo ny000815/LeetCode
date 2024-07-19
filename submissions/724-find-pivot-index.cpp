db7016c0500df073bff33a09ed3384ccefafac14
ny000815 <66006097+ny000815@users.noreply.github.com>
2024-07-19 12:36:03 +0000

    Sync LeetCode submission - Find Pivot Index

---
+++ submissions/724-find-pivot-index.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int pivotIndex(vector<int>& nums) {
+        int totalSum = 0;
+        for (int i = 0; i < nums.size(); i++){
+            totalSum += nums[i];
+        }
+        int leftSum = 0;
+        for (int i = 0; i < nums.size(); i++){
+            if (leftSum == totalSum -　leftSum - nums[i]) return i;
+            leftSum += nums[i];
+        }
+        return -1;
+    }
+};
