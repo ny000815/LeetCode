43ad5512f5012b4573d062516a191b2dfdbed206
ny000815 <66006097+ny000815@users.noreply.github.com>
2026-01-19 12:32:50 +0000

    Sync LeetCode submission - Increasing Triplet Subsequence

---
+++ submissions/334-increasing-triplet-subsequence.py
@@ -0,0 +1,12 @@
+class Solution:
+    def increasingTriplet(self, nums: List[int]) -> bool:
+        min1 = float('inf')
+        min2 = float('inf')
+        for i in nums:
+            if i < min1:
+                min1 = i
+            elif i < min2 and i != min1:
+                min2 = i
+            elif min2 < i:
+                return True
+        return False
