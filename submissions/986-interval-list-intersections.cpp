dd3da5e040964a84f130317b6307879368141373
ny000815 <66006097+ny000815@users.noreply.github.com>
2026-06-27 20:24:56 +0000

    Sync LeetCode submission - Interval List Intersections

---
+++ submissions/986-interval-list-intersections.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<vector<int>> intervalIntersection(vector<vector<int>>& firstList, vector<vector<int>>& secondList) {
+        vector<vector<int>> result;
+        size_t i = 0, j = 0;
+        while(i < firstList.size() && j < secondList.size()) {
+            int start = max(firstList[i][0], secondList[j][0]);
+            int end = min(firstList[i][1], secondList[j][1]);
+            if (start <= end) result.push_back({start,end});
+            if (firstList[i][1] < secondList[j][1]) ++i;
+            else ++j;
+        }
+        return result;
+    }
+};
