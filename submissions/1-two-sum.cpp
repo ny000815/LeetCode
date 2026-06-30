7020f9166bf94ee7086c34f3bb70bf7626e87506
ny000815 <66006097+ny000815@users.noreply.github.com>
2026-06-30 13:47:37 +0000

    Sync LeetCode submission - Two Sum

---
+++ submissions/1-two-sum.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> seen;
+        for(int i = 0; i < nums.size(); ++i) {
+            if (seen.find(target-nums[i]) != seen.end()) {
+                return {seen[target-nums[i]], i};
+            }
+            seen[nums[i]] = i;
+        }
+        vector<int> a;
+        return a;
+    }
+};
