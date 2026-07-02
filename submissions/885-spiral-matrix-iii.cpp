ce0e02dbd929862eba0a1f82ed446c72db7510b4
ny000815 <66006097+ny000815@users.noreply.github.com>
2026-07-02 15:03:14 +0000

    Sync LeetCode submission - Spiral Matrix III

--- submissions/885-spiral-matrix-iii.cpp
+++ submissions/885-spiral-matrix-iii.cpp
@@ -1,31 +1,29 @@
 class Solution {
 public:
     vector<vector<int>> spiralMatrixIII(int rows, int cols, int rStart, int cStart) {
-        int colD[] = {1,0,-1,0};
-        int rowD[] = {0,1,0,-1};
+        vector<vector<int>> result;
+        int dx[] = {1, 0, -1, 0};
+        int dy[] = {0, 1, 0, -1};
+        int directionIdx = 0;
 
-        vector<vector<int>> visited;
-        int currentRow = rStart;
-        int currentCol = cStart;
-
-        visited.push_back({currentRow, currentCol});
-
-        int directionIndex = 0;
         int legLength = 1;
 
-        while ((int)visited.size() < rows*cols) {
-            for(int leg = 0; leg < 2; ++leg) {
-                for(int step = 0; step < legLength; ++step) {
-                    currentRow += rowD[directionIndex];
-                    currentCol += colD[directionIndex];
-
-                    if(currentRow<0 || currentRow >= rows || currentCol<0 || currentCol >= cols) continue;
-                    visited.push_back({currentRow, currentCol});
+        int r = rStart, c = cStart;
+        result.push_back({rStart, cStart});
+        
+        while (result.size() < rows * cols) {
+            for(int i = 0; i < 2; ++i) {
+                for(int leg = 0; leg < legLength; ++leg) {
+                    r += dy[directionIdx];
+                    c += dx[directionIdx];
+                    if (r < 0 || r >= rows || c < 0 || c >= cols) continue;
+                    result.push_back({r,c});
                 }
-                directionIndex = (directionIndex + 1) % 4;
+                directionIdx = (directionIdx+1) % 4;
             }
             ++legLength;
         }
-        return visited;
+        return result;
+
     }
 };
