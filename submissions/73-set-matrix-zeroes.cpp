8a477e1ab057d260491b1e2d72f9d17a4f095457
ny000815 <66006097+ny000815@users.noreply.github.com>
2026-07-03 20:18:48 +0000

    Sync LeetCode submission - Set Matrix Zeroes

--- submissions/73-set-matrix-zeroes.cpp
+++ submissions/73-set-matrix-zeroes.cpp
@@ -1,20 +1,19 @@
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
-        int m = matrix.size(), n = matrix[0].size();
-        vector<bool> rows(m, false);
-        vector<bool> cols(n, false);
-        for (int i = 0; i < matrix.size(); i++) {
-            for (int j = 0; j < matrix[0].size(); j++) {
-                if (matrix[i][j] == 0) {
-                    rows[i] = true;
-                    cols[j] = true;
+        vector<int> cIdx(matrix.size(), 0);
+        vector<int> rIdx(matrix[0].size(), 0);
+        for(int i = 0; i < matrix.size(); ++i) {
+            for(int j = 0; j < matrix[0].size(); ++j) {
+                if (matrix[i][j] == 0){
+                    cIdx[i] = 1;
+                    rIdx[j] = 1;
                 }
             }
         }
-        for (int i = 0; i < matrix.size(); i++) {
-            for (int j = 0; j < matrix[0].size(); j++) {
-                if (rows[i] || cols[j]) matrix[i][j] = 0;
+        for(int i = 0; i < matrix.size(); ++i) {
+            for(int j = 0; j < matrix[0].size(); ++j){
+                if (cIdx[i] || rIdx[j]) matrix[i][j] = 0;
             }
         }
     }
