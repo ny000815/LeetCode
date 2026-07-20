d4dccb2945ca2ce591ebaba7136419b04f495662
ny000815 <66006097+ny000815@users.noreply.github.com>
2026-07-20 14:33:43 +0000

    Sync LeetCode submission - Merge Two Sorted Lists

--- submissions/21-merge-two-sorted-lists.cpp
+++ submissions/21-merge-two-sorted-lists.cpp
@@ -11,26 +11,16 @@
 class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
-        ListNode dummy;
-        ListNode* tail = &dummy;
-
-        while (list1 != nullptr && list2 != nullptr) {
-            if (list1->val <= list2->val) {
-                tail->next = list1;
-                list1 = list1->next;
-            } else {
-                tail->next = list2;
-                list2 = list2->next;
-            }
-            tail = tail->next;
-        }
-
-        if (list1 != nullptr) {
-            tail->next = list1;
-        } else {
-            tail->next = list2;
-        }
-
-        return dummy.next;
+        if(!list1) return list2;
+        if(!list2) return list1;
+        if(list1->val <= list2->val) {
+            list1->next = mergeTwoLists(list1->next, list2);
+            return list1;
+        } 
+        if(list2->val < list1->val) {
+            list2->next = mergeTwoLists(list1, list2->next);
+            return list2;
+        } 
+        return nullptr;
     }
 };
