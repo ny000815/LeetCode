2025e0fac1a7fbe9701e0659cc8fb567a6750292
ny000815 <66006097+ny000815@users.noreply.github.com>
2026-07-23 23:53:11 +0000

    Sync LeetCode submission - Invert Binary Tree

---
+++ submissions/226-invert-binary-tree.cpp
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+class Solution {
+public:
+    TreeNode* invertTree(TreeNode* root) {
+        if(!root) return nullptr;
+        queue<TreeNode*> que;
+        que.push(root);
+        while(!que.empty()) {
+            TreeNode* node = que.front();
+            que.pop();
+            swap(node->right, node->left);
+            if (node->left) que.push(node->left);
+            if (node->right) que.push(node->right);
+        }
+        return root;
+    }
+};
