ff8fbc973d21e3ad5df56f24a14f739a6944a800
ny000815 <66006097+ny000815@users.noreply.github.com>
2026-07-24 18:48:05 +0000

    Sync LeetCode submission - Contains Duplicate

--- submissions/217-contains-duplicate.cpp
+++ submissions/217-contains-duplicate.cpp
@@ -1,13 +1,6 @@
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-        unordered_set<int> seen;
-        const int n = nums.size();
-        seen.reserve(n);
-
-        for(int x : nums) {
-            if(!seen.insert(x).second) return true;
-        }
-        return false;
+        return unordered_set(nums.begin(), nums.end()).size() != nums.size();
     }
 };
