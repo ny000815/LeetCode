c672c3b48ad5b5325641a1e5ae65368383688adc
ny000815 <66006097+ny000815@users.noreply.github.com>
2026-08-02 02:45:52 +0000

    Sync LeetCode submission - Merge Sorted Array

--- submissions/88-merge-sorted-array.cpp
+++ submissions/88-merge-sorted-array.cpp
@@ -1,10 +1,10 @@
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        int idx = m + n - 1;
-        int i = m - 1, j = n - 1;
-        while(j >= 0) {
-            if(i >= 0 && nums1[i] > nums2[j]) nums1[idx--] = nums1[i--];
+        int idx = nums1.size()-1;
+        int i = m-1, j = n-1;
+        while(idx >= 0) {
+            if(j < 0 || i >= 0 && nums1[i] > nums2[j]) nums1[idx--] = nums1[i--];
             else nums1[idx--] = nums2[j--];
         }
     }
