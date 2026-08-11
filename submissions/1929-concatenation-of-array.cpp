dd424d3caa41b50afd64770ae24fc8609b05ba40
ny000815 <66006097+ny000815@users.noreply.github.com>
2026-08-11 18:45:18 +0000

    Sync LeetCode submission - Concatenation of Array

--- submissions/1929-concatenation-of-array.cpp
+++ submissions/1929-concatenation-of-array.cpp
@@ -1,7 +1,10 @@
 class Solution {
 public:
     vector<int> getConcatenation(vector<int>& nums) {
-        nums.insert(nums.end(), nums.begin(), nums.end());
+        int n = nums.size();
+        for(int i = 0; i < n; ++i) {
+            nums.push_back(nums[i]);
+        }
         return nums;
     }
 };
