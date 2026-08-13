df8a85571aa7e0520b872060a9f52c37b841c878
ny000815 <66006097+ny000815@users.noreply.github.com>
2026-08-13 14:20:06 +0000

    Sync LeetCode submission - Find And Replace in String

--- submissions/833-find-and-replace-in-string.cpp
+++ submissions/833-find-and-replace-in-string.cpp
@@ -2,21 +2,19 @@ class Solution {
 public:
     string findReplaceString(string s, vector<int>& indices, vector<string>& sources, vector<string>& targets) {
         int n = s.size(), k = indices.size();
-        vector<int> op(n, -1);
-        for(int i = 0; i < k; ++i){
-            if (s.compare(indices[i], sources[i].size(), sources[i]) == 0)
-                op[indices[i]] = i;
+        vector<int> match(n, -1);
+        for(int i = 0; i < k; ++i) {
+            if (s.compare(indices[i], sources[i].size(), sources[i]) == 0) match[indices[i]]=i;
         }
-        string res = "";
+        string res;
         for(int i = 0; i < n;) {
-            int idx = op[i];
-            if(idx != -1) {
-                res += targets[idx];
-                i += sources[idx].size();
+            if(match[i] != -1){
+                res += targets[match[i]];
+                i += sources[match[i]].size();
             }
             else {
-                res += s[i];
-                i++;
+                res += s[i]; 
+                i++; 
             }
         }
         return res;
