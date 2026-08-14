8bffca854daa8d26619fcbb2c846b1dad8eb715b
ny000815 <66006097+ny000815@users.noreply.github.com>
2026-08-14 14:51:19 +0000

    Sync LeetCode submission - Rotate Image

--- submissions/48-rotate-image.cpp
+++ submissions/48-rotate-image.cpp
@@ -1,14 +1,16 @@
 class Solution {
 public:
     void rotate(vector<vector<int>>& matrix) {
-        for (int i = 0; i < matrix.size(); i++) {
-            for (int j = i+1; j < matrix.size(); j++) {
+        int m = matrix.size();
+        int n = matrix[0].size();
+        for(int i = 0; i < m; ++i) {
+            for(int j = i+1; j<n; ++j) {
                 swap(matrix[i][j], matrix[j][i]);
             }
         }
-        for (int i = 0; i < matrix.size(); i++) {
-            for (int j = 0; j < matrix.size()/2; j++) {
-                swap(matrix[i][j], matrix[i][matrix.size()-1-j]);
+        for(int i = 0; i < m; ++i) {
+            for(int j = 0; j < n/2; ++j) {
+                swap(matrix[i][n-j-1], matrix[i][j]);
             }
         }
     }
