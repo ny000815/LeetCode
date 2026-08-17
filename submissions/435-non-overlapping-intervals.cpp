7fcc461aaed545be408475d93823cf2b5de52572
ny000815 <66006097+ny000815@users.noreply.github.com>
2026-08-17 20:14:53 +0000

    Sync LeetCode submission - Non-overlapping Intervals

--- submissions/435-non-overlapping-intervals.cpp
+++ submissions/435-non-overlapping-intervals.cpp
@@ -1,17 +1,12 @@
 class Solution {
 public:
     int eraseOverlapIntervals(vector<vector<int>>& intervals) {
-        int count = 0;
-        sort(intervals.begin(), intervals.end(), [](
-            const vector<int>& a, const vector<int>& b){
-            return a[1] < b[1];
-            });
-        int lastEnd = intervals[0][1];
+        sort(intervals.begin(), intervals.end(), [](const auto& a, const auto& b){ return a[1] < b[1];});
+        int ans = 0, end = intervals[0][1];
         for(int i = 1; i < intervals.size(); ++i) {
-            if (lastEnd > intervals[i][0]) {
-                ++count;
-            } else lastEnd = intervals[i][1];
+            if(intervals[i][0] < end) ++ans;
+            else end = max(end, intervals[i][1]);
         }
-        return count;
+        return ans;
     }
 };
