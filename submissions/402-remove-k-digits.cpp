b3d1b5a26bb03c8472cb9cd0b9d586b96ab39185
ny000815 <66006097+ny000815@users.noreply.github.com>
2026-08-19 18:24:02 +0000

    Sync LeetCode submission - Remove K Digits

---
+++ submissions/402-remove-k-digits.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    string removeKdigits(string num, int k) {
+        string ans = "";
+        for(int c : num) {
+            while (k > 0 && !ans.empty() && ans.back() > c) {
+                ans.pop_back();
+                --k;
+            }
+            ans.push_back(c);
+        }
+        while (k > 0) {
+            ans.pop_back();
+            --k;
+        }
+        int i = 0;
+        while(i < ans.size() && ans[i] == '0') ++i;
+        ans = ans.substr(i);
+        return ans.empty()? "0" : ans;
+    }
+};
