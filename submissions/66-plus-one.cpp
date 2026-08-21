1e41a1574c9084d2b57ad222918ff9e71225c038
ny000815 <66006097+ny000815@users.noreply.github.com>
2026-08-21 18:46:39 +0000

    Sync LeetCode submission - Plus One

--- submissions/66-plus-one.cpp
+++ submissions/66-plus-one.cpp
@@ -1,8 +1,8 @@
 class Solution {
 public:
     vector<int> plusOne(vector<int>& digits) {
-        int n = digits.size();
-        for(int i = n-1; i >= 0; --i) {
+        int n = digits.size()-1;
+        for(int i = n; i >= 0; --i) {
             if(digits[i] < 9) {
                 digits[i]++;
                 return digits;
