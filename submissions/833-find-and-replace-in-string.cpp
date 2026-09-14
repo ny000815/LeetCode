9795b70e27a0d09c8eb966038d30edad2284c647
ny000815 <66006097+ny000815@users.noreply.github.com>
2026-09-14 00:57:06 +0000

    Sync LeetCode submission - Find And Replace in String

--- submissions/833-find-and-replace-in-string.cpp
+++ submissions/833-find-and-replace-in-string.cpp
@@ -4,17 +4,18 @@ public:
         int n = s.size(), k = indices.size();
         vector<int> match(n, -1);
         for(int i = 0; i < k; ++i) {
-            if (s.compare(indices[i], sources[i].size(), sources[i]) == 0) match[indices[i]]=i;
+            if(s.compare(indices[i], sources[i].size(), sources[i]) == 0) {
+                match[indices[i]] = i;
+            }
         }
         string res;
         for(int i = 0; i < n;) {
-            if(match[i] != -1){
+            if(match[i] != -1) {
                 res += targets[match[i]];
                 i += sources[match[i]].size();
-            }
-            else {
-                res += s[i]; 
-                i++; 
+            }else {
+                res += s[i];
+                ++i;
             }
         }
         return res;
