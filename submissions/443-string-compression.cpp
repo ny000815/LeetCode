9b8e3ae5f1f7cbabd9cb68e0510d49e6021d56a6
ny000815 <66006097+ny000815@users.noreply.github.com>
2026-09-16 01:56:09 +0000

    Sync LeetCode submission - String Compression

--- submissions/443-string-compression.cpp
+++ submissions/443-string-compression.cpp
@@ -2,16 +2,16 @@ class Solution {
 public:
     int compress(vector<char>& chars) {
         int ans = 0;
-        for(int i = 0; i < chars.size();){
-            char letter = chars[i];
+        for(int i = 0; i < chars.size();) {
+            char c = chars[i];
             int count = 0;
-            while(i < chars.size() && chars[i] == letter) {
+            while(i < chars.size() && chars[i] == c) {
                 ++count;
                 ++i;
             }
-            chars[ans++] = letter;
-            if(count > 1) {
-                for(char c : to_string(count)) {
+            chars[ans++] = c;
+            if (count > 1) {
+                for (char c : to_string(count)) {
                     chars[ans++] = c;
                 }
             }
